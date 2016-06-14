5b53114238f0b5c349ce2faa751e1401f08d5a7e
kevin ventura <kevcodes@gmail.com>
2016-06-14 14:15:37 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+  num1/ num2
 end
+
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+
+def argue(phrase)
+  phrase
 end
 
-def greeting
+
+def greeting(name, location)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(con="cheese")
+  con
 end
+pizza_party()
